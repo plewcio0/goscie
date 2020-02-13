2688ae47f2c6c861efefa365024f9f4f25abb756
Daniel <ll8083129@gmail.com>
2020-02-13 14:35:04 +0100

    firebase init succes

--- initialize.js
+++
@@ -1,9 +0,0 @@
-var db = firebase.firestore();
-var zaproszeniRef = db.collection("Zaproszeni");
-
-zaproszeniRef.doc("M.Kac").set({
-    imie: "Milena",
-    Nazwisko: "Kacprowicz",
-    prefRosol: false,
-    prefKrem: true,
-});
---
+++ public/initialize.js
@@ -0,0 +1,9 @@
+// var db = firebase.firestore();
+// var zaproszeniRef = db.collection("Zaproszeni");
+
+// zaproszeniRef.doc("M.Kac").set({
+//     imie: "Milena",
+//     Nazwisko: "Kacprowicz",
+//     prefRosol: false,
+//     prefKrem: true,
+// });s
