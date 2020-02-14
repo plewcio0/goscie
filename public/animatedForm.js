08abc98f5886006305d836b6a4fa985eb6a1cf3c
Daniel <ll8083129@gmail.com>
2020-02-14 15:49:27 +0100

    feat: added guest iteration on second page

--- public/animatedForm.js
+++ public/animatedForm.js
@@ -1,7 +1,11 @@
 $('.nextButton').click(function() {
     guestsNumber = guestSelect.options[guestSelect.selectedIndex].value;
+    var innerHtml = "";
     for (let index = 0; index < guestsNumber; index++) {
-
+        innerHtml += `<div class="guestContainer">
+        <input class="guestContainer__Name" type="text" placeholder="Imię">
+        <input class="guestContainer__Name" type="text" placeholder="Nazwisko">
+    </div>`
     }
     $('.container').fadeTo(600, 0, function() {
         $(this).delay(600);
@@ -11,13 +15,10 @@ $('.nextButton').click(function() {
         </div>
 
         <div class="guestsContainer">
-            <div class="guestContainer">
-                <input class="guestContainer__Name" type="text" placeholder="Imię">
-                <input class="guestContainer__Name" type="text" placeholder="Nazwisko">
-            </div>
+            ${innerHtml}
         </div>
         <hr>
-        <div class="buttonContainer"><button class="nextButton">Dalej</button></div>
+        <div class="buttonContainer"><button class="nextButton2">Dalej</button></div>
     </div>`);
         $(this).fadeTo(600, 1);
     });
