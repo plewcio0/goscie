5f3000c340dc6cf2dd62adce4600c5d7a1877ef6
Daniel <plewa.daniel0@gmail.com>
2020-02-14 20:03:53 +0100

    feat: selectable second page options and css fix

--- public/animatedForm.js
+++ public/animatedForm.js
@@ -2,10 +2,43 @@ $('.nextButton').click(function() {
     guestsNumber = guestSelect.options[guestSelect.selectedIndex].value;
     var innerHtml = "";
     for (let index = 0; index < guestsNumber; index++) {
-        innerHtml += `<div class="guestContainer">
-        <input class="guestContainer__Name" type="text" placeholder="Imię">
-        <input class="guestContainer__Name" type="text" placeholder="Nazwisko">
-    </div>`
+        if (index == guestsNumber-1)
+        {
+            innerHtml += `<div class="guestContainer">
+            <div class="nameContainer">
+            <input class="guestContainer__Name" type="text" placeholder="Imię">
+            </div>
+            <div class="nameContainer">
+            <input class="guestContainer__Name" type="text" placeholder="Nazwisko">
+            </div>
+            <div class="ask">
+            <span>Czy potwierdzasz obecność?</span>
+            <select id="guestChoice" class="guestChoice">
+            <option class="guestSelect" value="Tak">Tak, będę!</option>
+            <option class="guestSelect" value="Nie">Niestety, nie mogę.</option>
+          </select>
+          </div>
+        </div>`
+        }
+        
+        else
+        {
+            innerHtml += `<div class="guestContainer">
+            <div class="nameContainer">
+            <input class="guestContainer__Name" type="text" placeholder="Imię">
+            </div>
+            <div class="nameContainer">
+            <input class="guestContainer__Name" type="text" placeholder="Nazwisko">
+            </div>
+            <div class="ask">
+            <span>Czy potwierdzasz obecność?</span>
+            <select id="guestChoice" class="guestChoice">
+            <option class="guestSelect" value="Tak">Tak, będę!</option>
+            <option class="guestSelect" value="Nie">Niestety, nie mogę.</option>
+          </select>
+          </div>
+        </div><hr class="guestLine">`
+        }
     }
     $('.container').fadeTo(600, 0, function() {
         $(this).delay(600);
