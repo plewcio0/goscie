c520c98cabc3e98373f537519f11ca1a66d0da0f
Daniel <ll8083129@gmail.com>
2020-02-18 14:11:42 +0100

    refractoring

--- public/animatedForm.js
+++ public/animatedForm.js
@@ -1,4 +1,4 @@
-$('.nextButton').click(function () {
+$('.nextButton').click(function() {
     guestsNumber = guestSelect.options[guestSelect.selectedIndex].value;
     var innerHtml = "";
     for (let index = 0; index < guestsNumber; index++) {
@@ -18,9 +18,7 @@ $('.nextButton').click(function () {
           </select>
           </div>
         </div>`
-        }
-
-        else {
+        } else {
             innerHtml += `<div class="guestContainer">
             <div class="nameContainer">
             <input class="guestContainer__Name" type="text" placeholder="Imię" id="imie${index}">
@@ -38,7 +36,7 @@ $('.nextButton').click(function () {
         </div><hr class="guestLine">`
         }
     }
-    $('.container').fadeTo(600, 0, function () {
+    $('.container').fadeTo(600, 0, function() {
         $(this).delay(600);
         $(this).html(`        <div class="wrapper2">
         <div class="header" id="dashboard" role="alert">
@@ -51,16 +49,24 @@ $('.nextButton').click(function () {
         <hr>
         <div class="buttonContainer"><button class="nextButton2">Dalej</button></div>
     </div>`);
-        $(this).fadeTo(600, 1, function () {
+        $(this).fadeTo(600, 1, function() {
             //
             $('.guestContainer__Name').on('input', (e) => {
                 if (e.target.classList.contains('wrong')) {
                     e.target.classList.remove('wrong');
                 }
             })
-            $('.nextButton2').click(function () {
+            $('.nextButton2').click(function() {
                 // po kliknieciu w przycisk
-                CheckIfEmpty();
+                var sss = $(this).parent();
+                var guestsContainer = $(this).parent().prevAll(".guestsContainer")
+                if (CheckIfEmpty(guestsContainer)) {
+                    console.log("pustka gdziess")
+                } else {
+                    // if (guestcontainers.contains(ListaOsobZOsobami)) -- jak jest ktos z nieznanych
+                    // else if  -- jak wszyscy znani
+                    //  dorobic
+                }
             })
         });
 
@@ -68,36 +74,47 @@ $('.nextButton').click(function () {
 
 });
 
-
-function CheckIfEmpty() {
-    $('.guestContainer').each(function (index) {
-        var im = $(this).children(":first").children();
-        var nz = $(this).children(":first").next().children();
-        if (im.val() == "") { //imie
-            im.addClass('wrong');
-        }
-        if (nz.val() == "") { //nazwisko
-            nz.addClass('wrong');
-        }
-        else { // jesli oba pola na imie i nazwisko nie są puste \/
-            var choice = $(this).children('.ask').children('#guestChoice').val();   // sprawdzenie czy wybral tak czy nie
-            AddConfirmedGuest(im.val(), nz.val(), choice);
-        }
+function CheckIfEmpty(guestsContainer) {
+    var empty = false;
+    $(guestsContainer).children('.guestContainer').each(function(index, element) {
+        $(element).children('.nameContainer').each(function(index, element) {
+            if ($(element).children().val() == "") {
+                $(element).children().addClass('wrong');
+                empty = true;
+            }
+        })
     })
-
+    return empty;
 }
+
+// function CheckIfEmpty() {
+//     $('.guestContainer').each(function(index) {
+//         var im = $(this).children(":first").children();
+//         var nz = $(this).children(":first").next().children();
+//         if (im.val() == "") { //imie
+//             im.addClass('wrong');
+//         }
+//         if (nz.val() == "") { //nazwisko
+//             nz.addClass('wrong');
+//         } else { // jesli oba pola na imie i nazwisko nie są puste \/
+//             var choice = $(this).children('.ask').children('#guestChoice').val(); // sprawdzenie czy wybral tak czy nie
+//             AddConfirmedGuest(im.val(), nz.val(), choice);
+//         }
+//     })
+
+// }
 
 
 function AddConfirmedGuest(im, nz, czy) {
     invitedRef.where("Imie", "==", im.capitalize()).where("Nazwisko", "==", nz.capitalize())
         .get()
-        .then(function (querySnapshot) {
+        .then(function(querySnapshot) {
             if (querySnapshot.empty) {
                 console.log("nie ma cie w bazie")
             } else {
-                querySnapshot.forEach(function (doc) {
+                querySnapshot.forEach(function(doc) {
                     // doc.data() is never undefined for query doc snapshots
-                    console. log(doc.id, " => ", doc.data());
+                    console.log(doc.id, " => ", doc.data());
                     invitedRef.doc(doc.id).update({
                         czyPrzyjdzie: czy
                     })
@@ -105,12 +122,12 @@ function AddConfirmedGuest(im, nz, czy) {
             }
 
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log("Error getting documents: ", error);
         });
 }
 
 
-String.prototype.capitalize = function () {
+String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 }
