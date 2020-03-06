8e021f115f3d8b5038a8e97e543d083e887b1412
Daniel <plewa.daniel0@gmail.com>
2020-03-06 14:51:31 +0100

    feat: fixed hosting to milenaidaniel2020

--- public/adminList.js
+++ public/adminList.js
@@ -1,30 +1,31 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var list = [];
-invitedRef
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            console.log(doc.id, " => ", doc.data());
-            CreateList(doc.data());
-        });
-        $('.container').append(list);
 
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
 
 function CreateList(guest) {
     if (guest.zKim == null) {
-        list.push(`<div>${guest["Imie"]} ${guest["Nazwisko"]}</div>`)
+        if (guest.czyPrzyjdzie == "Tak") {
+            list.push(`<div class="przyjdzie">${guest["Imie"]} ${guest["Nazwisko"]}</div>`)
+        } else if (guest.czyPrzyjdzie == "Nie") {
+            list.push(`<div class="niePrzyjdzie">${guest["Imie"]} ${guest["Nazwisko"]}</div>`)
+        } else {
+            list.push(`<div>${guest["Imie"]} ${guest["Nazwisko"]}</div>`)
+        }
     } else {
-        list.push(`<div>${guest["Imie"]} ${guest["Nazwisko"]} (${guest.zKim})</div>`)
+        if (guest.czyPrzyjdzie == "Tak") {
+            list.push(`<div class="przyjdzie">${guest["Imie"]} ${guest["Nazwisko"]} (${guest.zKim})</div>`)
+        } else if (guest.czyPrzyjdzie == "Nie") {
+            list.push(`<div class="niePrzyjdzie">${guest["Imie"]} ${guest["Nazwisko"]} (${guest.zKim})</div>`)
+        } else {
+            list.push(`<div>${guest["Imie"]} ${guest["Nazwisko"]} (${guest.zKim})</div>`)
+        }
     }
 }
 
-
 db.collection("Invited")
-    .onSnapshot(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
+    .onSnapshot(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
             if (doc.data().czyPrzyjdzie == "Tak") {
                 UpdateList("Tak", doc.data());
             } else if (doc.data().czyPrzyjdzie == "Nie") {
@@ -41,6 +42,9 @@ function UpdateList(czy, kto) {
     } else {
         x = `${kto.Imie} ${kto.Nazwisko} (${kto.zKim})`;
     }
+    $(list).each(function (ind, elem) {
+        console.log(elem);
+    })
     $('.container').find('*').each((ind, elem) => {
         if (elem.innerText == x) {
             if (czy == "Tak") {
@@ -53,3 +57,65 @@ function UpdateList(czy, kto) {
         }
     })
 }
+
+
+
+document.querySelector('.Loguj').addEventListener('click', () => {
+    var login = $('.login__input').val();
+    var haslo = $('.haslo__input').val();
+    console.log(login)
+    console.log(haslo)
+    Zaloguj(login, haslo);
+})
+
+function ClearList() {
+    invitedRef.get().then(function (querySnapshot) {
+        querySnapshot.forEach(doc => {
+            doc.ref.update({
+                czyPrzyjdzie: firebase.firestore.FieldValue.delete()
+            })
+        });
+    })
+}
+
+function Zaloguj(login, haslo) {
+    firebase.auth().signInWithEmailAndPassword(login, haslo).catch(function (err) {
+        console.log('error loggin')
+    })
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+        console.log('logged');
+        $('.container').empty();
+        invitedRef
+            .get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    console.log(doc.id, " => ", doc.data());
+                    CreateList(doc.data());
+                });
+                $('.container').append(list);
+                $('.container').append(`<button class="clearGuests--js">Wyczyść liste</button>`);
+                document.querySelector('.clearGuests--js').addEventListener('click', () => {
+                    ClearList();
+                })
+            })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+    } else {
+        console.log('not logged');
+        // User is signed out.
+        // ...
+    }
+});
